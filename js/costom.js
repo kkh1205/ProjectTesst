4698b40a326a1b7ba4633c4f63fa38a61650ccf3
DESKTOP-N146Q0B\esm91 <esm9105@naver.com>
2021-08-05 11:30:21 +0900

    /성민/ 이미지슬라이드 및 슬라이드 컨트롤버튼 추가

--- js/costom.js
+++ js/costom.js
@@ -1,9 +1,20 @@
-
+//sm. 슬라이드 이미지 작동 스크립트(swiper)
   var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
+    autoplay: {
+        //시간 1000 이 1초
+        delay: 2500,
+        disableOnInteraction: false,
+       },
+    //    effect: 'cube',
+    //    cubeEffect: {
+    //      slideShadows: false,
+    //      shadow : false,
+    //    },
+       speed: 1000,
   });
 
 
