0b8d69e4715b14d65b7a3fb3855faa7ccec0d161
PC-082\user <ksoy0421@gmail.com>
2021-08-17 13:33:55 +0900

    /소연/스크롤 정리업로드 이해완료

--- js/customsy.js
+++ js/customsy.js
@@ -1,28 +1,3 @@
-// // 스크롤 이벤트 아직 이해못함 시작
-
-// function isElementUnderBottom(elem, triggerDiff) {
-//     const { top } = elem.getBoundingClientRect();
-//     const { innerHeight } = window;
-//     return top > innerHeight + (triggerDiff || 0);
-//   }
-  
-//   function handleScroll() {
-//     const elems = document.querySelectorAll('.up-on-scroll');
-//     elems.forEach(elem => {
-//       if (isElementUnderBottom(elem, -20)) {
-//         elem.style.opacity = "0";
-//         elem.style.transform = 'translateY(70px)';
-//       } else {
-//         elem.style.opacity = "1";
-//         elem.style.transform = 'translateY(0px)';
-//       }
-//     })
-//   }
-  
-//   window.addEventListener('scroll', handleScroll);
-
-//   // 스크롤 이해못함 끝
-
 
 
   $(window).scroll(function(){//스크롤 움직이는거 감지해서 펑션 작동
@@ -31,74 +6,56 @@
     console.log(scrollPosition);
   
     //sm.여기서부터는 스크롤 내리면 나타내는 효과들
-    if(scrollPosition >= 100){//스크롤값 200이상이면 페이드인업 해서 애니메이션 작동
-      //ex. movingtasteBox('맛' 이라고 적혀있는 상자)의 id를 검색해서 그 id에 속한 class를 애니메이션이 들어가있는 class로 변경해버림
-      document.getElementById('movingtasteBox').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
+
     if(scrollPosition >= 200){
       document.getElementById('tastePart1').className = 'row animate__animated animate__fadeInUp';
     }
     if(scrollPosition >= 900){
       document.getElementById('tastePart2').className = 'row animate__animated animate__fadeInUp';
     }
-    if(scrollPosition >= 1800){
-      document.getElementById('movingganjiBox').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 2000){
-      document.getElementById('ganjiExplain1').className = 'row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 2600){
-      document.getElementById('ganjiExplain2').className = 'row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 3000){
-      document.getElementById('ganjiExplain3').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 3800){
-      document.getElementById('movingrestBox').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 4000){
-      document.getElementById('restExplain1').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
-    if(scrollPosition >= 4500){
-      document.getElementById('restExplain2').className = 'container-fluid row animate__animated animate__fadeInUp';
-    }
+
   //스크롤 내리면 나타내는 효과들 끗
   
   
   //여기서부터는 스크롤 올리면 없어지기
   
-    if(scrollPosition < 200){//스크롤값 200 미만이면 페이드아웃다운 해서 애니메이션 작동
-      //ex. movingtasteBox('맛' 이라고 적혀있는 상자)의 id를 검색해서 그 id에 속한 class를 애니메이션이 들어가있는 class로 변경해버림
-      document.getElementById('movingtasteBox').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
+
     if(scrollPosition < 500){
       document.getElementById('tastePart1').className = 'row animate__animated animate__fadeOutDown';
     }
     if(scrollPosition < 900){
       document.getElementById('tastePart2').className = 'row animate__animated animate__fadeOutDown';
     }
-    if(scrollPosition < 1800){
-      document.getElementById('movingganjiBox').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 2000){
-      document.getElementById('ganjiExplain1').className = 'row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 2600){
-      document.getElementById('ganjiExplain2').className = 'row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 3000){
-      document.getElementById('ganjiExplain3').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 3800){
-      document.getElementById('movingrestBox').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 4000){
-      document.getElementById('restExplain1').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
-    if(scrollPosition < 4500){
-      document.getElementById('restExplain2').className = 'container-fluid row animate__animated animate__fadeOutDown';
-    }
+
   
   
   
   });
+
+
+// sy.스크롤 이벤트 시작
+// 스크린 아래에 있으면 true를 반환, 스크린보다 위에 있으면 false를 반환함.
+// 여기서 element는 사진, 글 등을 의미
+
+function isElementUnderBottom(elem, triggerDiff) {
+  const { top } = elem.getBoundingClientRect();
+  const { innerHeight } = window;
+  return top > innerHeight + (triggerDiff || 0);
+}
+
+function handleScroll() {
+  const elems = document.querySelectorAll('.up-on-scroll');
+  elems.forEach(elem => {
+    if (isElementUnderBottom(elem, -20)) {
+      elem.style.opacity = "0";                           // 각 element가 화면보다 아래에 있다면
+      elem.style.transform = 'translateY(70px)';          // style 적용
+    } else {
+      elem.style.opacity = "1";                           // 각 element가 화면 안에 있다면
+      elem.style.transform = 'translateY(0px)';           // style 적용
+    }
+  })
+}
+
+window.addEventListener('scroll', handleScroll);
+
+// sy.스크롤 이벤트 끝
